31487435aecb418074680f7c1dccafb6da7b9d55
wikum <wikum@xcendant.com>
2017-11-10 12:28:30 +0530

    add Employee controller,model and view

---
+++ src/Employee/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee.Models
+{
+    public class Employee
+    {
+        public string EmployeeNumber { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeAddress { get; set; }
+        public string EmployeeDOB { get; set; }
+        public string EmployeeContact { get; set; }
+        public string EmployeeImage { get; set; }
+
+    }
+}
